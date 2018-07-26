1d6a8a2b1f6f49a628e2e24d6cbb63704dfbc295
root <root@DESKTOP-NM1J0DE.localdomain>
2018-07-27 05:33:42 +0800

    add permit and forbid scroll function

--- EugeneOptimization.js
+++ EugeneOptimization.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Optimize Browser
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.1.0
 // @description  Have a better experience to use browser.
 // @author       Eugene Liu
 // @match        *://*/*
